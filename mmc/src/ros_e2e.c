d4cb134c6ad1d51329571c31a22ef2707b0f431c
Patrick Hao <patrick.hao95@gmail.com>
2023-06-11 20:06:08 +1000

    Wrote more tests for rcutils_is_file.

--- mmc/src/ros_e2e.c
+++ mmc/src/ros_e2e.c
@@ -22,8 +22,9 @@ result_t ros_e2e_rcutils_is_file(void) {
     bool is_file = rcutils_is_file("i_dont_exist.txt");
     assert(false == is_file);
 
-    /* Creating a new file. */
     FRESULT res;
+
+    /* Mount FS. */
     FATFS fs;
     res = f_mount(&fs, "", 0);
     if (res != FR_OK) {
@@ -31,14 +32,22 @@ result_t ros_e2e_rcutils_is_file(void) {
     }
     assert(FR_OK == res);
 
+    /* Delete file. */
+    char const *file_path = "ros_e2e_rcutils_is_file.txt";
+    res = f_unlink(file_path);
+    if (res != FR_OK) {
+        log_info("Error deleting file with res of %d.", res);
+    }
+
+    /* Creating file. */
     FIL fp = {0};
-    char const *path = "ros_e2e_rcutils_is_file.txt";
-    res = f_open(&fp, path, FA_WRITE | FA_CREATE_ALWAYS);
+    res = f_open(&fp, file_path, FA_WRITE | FA_CREATE_ALWAYS);
     if (res != FR_OK) {
         log_info("Error opening file with res of %d.", res);
     }
     assert(FR_OK == res);
 
+    /* Close file. */
     res = f_close(&fp);
     if (res != FR_OK) {
         log_info("Error closing file with res of %d.", res);
@@ -47,8 +56,23 @@ result_t ros_e2e_rcutils_is_file(void) {
 
     /* Checking `rcutils` returns true for file that does exist. */
     log_info("Checking `rcutils` returns true for file that does exist.");
-    is_file = rcutils_is_file(path);
+    is_file = rcutils_is_file(file_path);
     assert(true == is_file);
+
+    /* Delete the directory first. */
+    char const *dir_path = "ros_e2e_rcutils_is_file_dir";
+    res = f_unlink(dir_path);
+    if (res != FR_OK) {
+        log_info("Error deleting directory with res of %d.", res);
+    }
+
+    /* Create a new directory. */
+    bool res_mkdir = rcutils_mkdir(dir_path);
+    assert(true == res_mkdir);
+
+    /* Checking `rcutils` returns false for directory we just created. */
+    is_file = rcutils_is_file(dir_path);
+    assert(false == is_file);
 
     log_info("Finished ros_e2e_rcutils_is_file().");
     return result_ok();
